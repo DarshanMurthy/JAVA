0136a67f04003cede51dc7ac3c65469216a7821a
Darshu <darshants.darshan1@gmail.com>
2017-08-17 07:24:27 -0700

    Node is added

---
+++ Tree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node left,right;
+}
+class BinaryTree{
+    Node root;
+
+
+}
+
+class Tree{
+    public static void main(String args[]){
+        System.out.println("Hello");
+        BinaryTree tree = new BinaryTree();
+        
+    }
+}
