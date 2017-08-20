2f6fbabeba69515cc46c469c3b4a1dc7f4657f12
Darshu <darshants.darshan1@gmail.com>
2017-08-20 09:17:16 -0700

    inorder traversal

--- Tree.java
+++ Tree.java
@@ -1,19 +1,39 @@
 import java.util.*;
-
 class Node{
-    int data;
+    int value ;
     Node left,right;
-}
-class BinaryTree{
-    Node root;
-
-
-}
-
-class Tree{
-    public static void main(String args[]){
-        System.out.println("Hello");
-        BinaryTree tree = new BinaryTree();
-        
+    public Node(int data){
+        data=value;
+        left=right=null;
     }
+}
+
+
+public class Tree{
+    Node root;
+    Tree(){
+        root=null;
+    }
+
+    void preOrder(Node node){
+        if(node == null) return ;
+        System.out.println(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    
+public static void main(String args[]){
+    Tree tree = new Tree();
+    tree.root = new Node(10);
+    tree.root.left = new Node(20);
+    tree.root.right = new Node(30);
+    tree.preOrder(tree.root);
+
+    // HashMap<Integer,String> map = new HashMap<Integer,String>();
+    // map.put(123,"node");
+    // for(Map.Entry m: map.entrySet()){
+    //     System.out.println(m.getKey());
+    // }
+}
 }
---
+++ binaryTree.java
@@ -0,0 +1,28 @@
+class Node{
+    Node left, right;
+    int key;
+    Node(int data){
+        key = data;
+    }
+};
+public class binaryTree{
+    Node root;
+    void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.println(root.key);
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        binaryTree Tree = new binaryTree();
+        Tree.root = new Node(10);
+        Tree.root.left = new Node(-1);
+        Tree.root.right = new Node(90);
+        Tree.preOrder(Tree.root);
+
+    }
+}
